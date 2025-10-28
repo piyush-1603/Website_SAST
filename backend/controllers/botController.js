a0073d3bf38945d72522a5312902e31d3ea56af1
Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>
2025-10-28 13:33:50 +0530

    setup backend
    Signed-off-by: Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>

---
+++ backend/controllers/botController.js
@@ -0,0 +1,14 @@
+const { genRes } = require("../utils/botService");
+
+const returnResponse = async (req, res) => {
+  try {
+    const { msg, history } = req.body;
+    const response = await genRes(msg, history);
+    return res.status(200).json({ msg: response });
+  } catch (err) {
+    console.error("Error generating response:", err);
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+};
+
+module.exports = returnResponse;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
+const botRouter = require("./routes/botRoutes");
+
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend is Running!');
+app.use("/bot", botRouter);
+
+app.get("/", (req, res) => {
+  res.send("Backend is Running!");
 });
 
 app.listen(port, () => {
