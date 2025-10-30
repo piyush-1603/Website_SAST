9d0ddb3ef93627c9415e49796a4b67834981a3e2
piyush-1603 <piyush16032006@gmail.com>
2025-10-30 15:19:14 +0530

    Add Footer component to Landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,6 +10,7 @@ import videosource5 from "../Landing_media/spacerealastro.mov";
 import img1 from "../Landing_media/All-possible-through-our-state-of-the-art-space-service-2160x2170-4-2160x1660.webp";
 import img2 from "../Landing_media/frequent_lines.webp";
 import helmet_png from "../Landing_media/helm.jpg";
+import Footer from "./footer";
 import useLenis from "../utils/lenis";
 import useSettings from "../hooks/UseSettings";
 
@@ -332,6 +333,7 @@ const Landing = () => {
           </a>
         </section>
       </main>
+      <Footer />
     </>
   );
 };
