566ec654150db1a9b23c56ac2c4c56ea82277041
MankuDada <mayanksharma1@Mayanks-MacBook-Pro-9.local>
2025-10-31 11:30:45 +0530

    basic folder made of backend

--- backend/routes/botRoutes.js
+++ backend/routes/botRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const returnResponse = require("../controllers/botController");
 const botRouter = express.Router();
+
 botRouter.post("/", returnResponse);
-module.exports = botRouter;
+
+module.exports = {
+    botRouter
+};
