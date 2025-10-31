13d98399f0308c9bace03a1a05a5f48c52e7f59c
Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>
2025-10-31 14:31:23 +0530

    add fallback images
    Signed-off-by: Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>

--- src/components/AstronomyNews.jsx
+++ src/components/AstronomyNews.jsx
@@ -88,7 +88,7 @@ export default function AstronomyNews() {
   const handleRefresh = async () => {
     setIsRefreshing(true);
     seenIds.current.clear();
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     await queryClient.invalidateQueries({ queryKey: ["astronomyNews"] });
     setIsRefreshing(false);
   };
@@ -101,13 +101,31 @@ export default function AstronomyNews() {
     }
   };
 
+  function getRandomAstronomyImage() {
+    const images = [
+      "https://images-assets.nasa.gov/image/PIA12235/PIA12235~orig.jpg",
+      "https://apod.nasa.gov/apod/image/1901/IC405_Abolfath_3952.jpg",
+      "https://images-assets.nasa.gov/image/PIA04921/PIA04921~orig.jpg",
+      "https://apod.nasa.gov/apod/image/1807/NGC6744-HaLRGBpugh1024.jpg",
+      "https://images-assets.nasa.gov/image/PIA03606/PIA03606~orig.jpg",
+      "https://apod.nasa.gov/apod/image/1701/OrionNebula_Hubble_960.jpg",
+      "https://apod.nasa.gov/apod/image/1902/MilkyWayPanorama_Merzlyakov_2000.jpg",
+      "https://apod.nasa.gov/apod/image/1709/LagoonTrifid_Vargas_1824.jpg",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+  }
+
   return (
     <div className="pt-44 md:pt-56 px-0">
       <div className="astronomy-news-container">
         <header className="astronomy-header">
           <h1 className="astronomy-title">Astronomy News</h1>
           <button
-            className={`refresh-button ${isRefreshing || isFetching ? "loading" : ""}`}
+            className={`refresh-button ${
+              isRefreshing || isFetching ? "loading" : ""
+            }`}
             onClick={handleRefresh}
             disabled={isRefreshing || isFetching}
           >
@@ -157,7 +175,7 @@ export default function AstronomyNews() {
                     className="news-image"
                     loading="lazy"
                     onError={(e) => {
-                      e.target.src = "/vite.svg";
+                      e.target.src = getRandomAstronomyImage();
                     }}
                   />
                 </div>
